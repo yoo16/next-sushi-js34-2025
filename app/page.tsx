d6bdc02462169a5da233c767e0d3fa8b2a323b81
yoo16 <yohei.yoshikawa@gmail.com>
2025-06-25 10:11:19 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="flex flex-col bg-gradient-to-b from-white to-sky-200 items-center justify-center min-h-screen">
-            サイトロゴ画像
+            <img src="/images/site_logo.png" 
+                className="w-[420px] mb-12"
+                alt="はる寿司" />
             <Link
                 href="/menu"
                 className="px-6 py-3 bg-sky-600 text-white rounded-xl shadow hover:bg-sky-500 transition">
