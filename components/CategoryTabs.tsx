47a256b467b484ce857ad6f70fd37e186b16d418
yoo16 <yohei.yoshikawa@gmail.com>
2025-06-25 10:33:14 +0900

    update

--- components/CategoryTabs.tsx
+++ components/CategoryTabs.tsx
@@ -11,26 +11,12 @@ type Props = {
 export default function CategoryTabs({ categories, current, onSelect }: Props) {
     return (
         <div className="flex flex-wrap justify-center mb-6">
-            <button
-                className={`flex-1 px-4 py-2 rounded m-1 transition cursor-pointer bg-sky-600 text-white`}
-            >カテゴリ1
-            </button>
-            <button
-                className={`flex-1 px-4 py-2 rounded m-1 transition cursor-pointer bg-white-600 text-sky`}
-            >カテゴリ2
-            </button>
-            <button
-                className={`flex-1 px-4 py-2 rounded m-1 transition cursor-pointer bg-white-600 text-sky`}
-            >カテゴリ3
-            </button>
-            <button
-                className={`flex-1 px-4 py-2 rounded m-1 transition cursor-pointer bg-white-600 text-sky`}
-            >カテゴリ4
-            </button>
-            <button
-                className={`flex-1 px-4 py-2 rounded m-1 transition cursor-pointer bg-white-600 text-sky`}
-            >カテゴリ5
-            </button>
+            {categories.map((cat) => (
+                <button
+                    className={`flex-1 px-4 py-2 rounded m-1 transition cursor-pointer bg-sky-600 text-white`}
+                >カテゴリ1
+                </button>
+            ))}
         </div>
     );
 }
