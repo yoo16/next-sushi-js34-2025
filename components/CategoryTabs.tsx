6864d8e4ec14657a9a63e7f78a9ac0acc5e17159
yoo16 <yohei.yoshikawa@gmail.com>
2025-06-25 10:41:27 +0900

    update

--- components/CategoryTabs.tsx
+++ components/CategoryTabs.tsx
@@ -13,11 +13,15 @@ export default function CategoryTabs({ categories, current, onSelect }: Props) {
         <div className="flex flex-wrap justify-center mb-6">
             {categories.map((cat) => (
                 <button
-                    className={`flex-1 px-4 py-2 rounded m-1 transition cursor-pointer bg-sky-600 text-white`}
+                    className={`flex-1 px-4 py-2 rounded m-1 transition cursor-pointer
+            ${current?.id === cat.id
+                            ? 'bg-sky-600 text-white'
+                            : 'bg-white hover:bg-sky-100'}`}
                 >
                     {cat.name}
                 </button>
-            ))}
-        </div>
+            ))
+            }
+        </div >
     );
 }
