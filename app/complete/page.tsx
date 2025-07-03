8d6dbe09d027786c526b2773e9994098dfb96dcf
yoo16 <yohei.yoshikawa@gmail.com>
2025-07-03 16:55:55 +0900

    update

--- app/complete/page.tsx
+++ app/complete/page.tsx
@@ -1,11 +1,41 @@
 'use client';
 
 import TitleLink from "@/components/TitleLink";
+import { billed } from "@/lib/api";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function CompletePage() {
-    // ローカルストレージから visit_id を削除
-    localStorage.removeItem('visit_id');
+
+    useEffect(() => {
+        // visit_idを取得して、会計処理を実行
+        const visitId = Number(localStorage.getItem('visit_id'));
+        bill(visitId);
+    }, []);
+
+    /**
+     * 請求処理
+     * 
+     * @param visitId 
+     * @returns 
+     */
+    async function bill(visitId: number) {
+        try {
+            const result = await billed(visitId)
+            if (result.error) {
+                console.log('チェックアウト失敗:', result.error);
+                return;
+            } else {
+                console.log('チェックアウト成功:', result);
+                // ローカルストレージからvisit_idとseat_idを削除
+                localStorage.removeItem('visit_id');
+            }
+        } catch (error) {
+            console.error('会計処理中にエラー:', error);
+        } finally {
+
+        }
+    }
 
     return (
         <div className="bg-sky-50 text-gray-800 flex items-center justify-center min-h-screen">
